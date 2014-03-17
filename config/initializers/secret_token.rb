ddb0bd110bfd85852c22dfa512d577aaedb61755
Jim Haburne <jhaburne@comcast.net>
2014-03-17 01:47:27 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handicap::Application.config.secret_token = '1ee88047fd12efca542da8d73d654d00d400fada7d247bd9c9e4db2fde9b299fa8625211edd538ffefcc9627722cdc7d6f24d981056d4ad09c696b35f3d39ae7'
